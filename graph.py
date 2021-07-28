6760861ca9276488198f8a8cf8ea1d1a93aec7cc
Grellik <73647619+Grellik@users.noreply.github.com>
2021-07-28 08:15:25 +0200

    Update graph.py

--- graph.py
+++ graph.py
@@ -10,50 +10,16 @@ def graph_time(df):
 
     # Отображение кол-ва багов каждый день в течении указанного месяца
     if choice == 1:
-
-        month = int(input("month (as number) = "))
+        
+        month = []
+        month.append(int(input("month (as number) = ")))
         year = int(input("year (as 4 digit number) ="))
         
         bug_num = []
         day_num = []
         bugs = 0
-        
-        # Поиск нужной даты начиная с самой поздней
-        for index, row in df.iterrows():
-            bugs = 0
-            cd = row["creation date"].split("T")
-            cd = cd[0].split("-")
 
-            # Конец цикла как только он вышел за предел искомой даты, дабы не терять время при работе-
-            #- с файлом большого обьема
-            if year > int(cd[0]) or month > int(cd[1]):
-                break
-
-            # Сохранение каждого полученого бага и даты его получения в списки
-            if year == int(cd[0]) and month == int(cd[1]):
-                bugs += 1
-            bug_num.append(bugs)
-            day_num.append(str(cd[2]))
-        
-        # Реверс списков для отображения "прошлое" -> "будущее" по оси х
-        day_num.reverse()
-        bug_num.reverse()
-        
-        graph_data = {
-            "bugs" : bug_num,
-            "day" : day_num
-        }
-        
-        # Создание датафрейма для графика
-        gdf = pd.DataFrame(graph_data, columns=["bugs", "day"])
-        # Если на один день приходится несколько багов, одни объеденяются в одну строку
-        gdf = gdf.groupby("day").agg({"bugs":'sum'}).reset_index()
-        
-        gdf.plot(x ="day", y="bugs", kind = 'bar')
-        plt.show()
-
-    # Отображение кол-ва багов каждый день в течении указанного квартала
-    else:
+    elif choice == 2:
 
         quarter = int(input("quarter (1-4) = "))
         year = int(input("year (as 4 digit number) = "))
@@ -65,55 +31,63 @@ def graph_time(df):
             month = [7,8,9]
         else:
             month = [10,11,12]
-        
-        bug_num = []
-        day_num = []
+
+    # Поиск нужной даты начиная с самой поздней
+    for index, row in df.iterrows():
         bugs = 0
-        
-        for index, row in df.iterrows():
-            bugs = 0
-            cd = row["creation date"].split("T")
-            cd = cd[0].split("-")
-            if year > int(cd[0]) and int(cd[1] not in month):
-                break
-            if year == int(cd[0]) and int(cd[1]) in month:
-                bugs += 1
-            bug_num.append(bugs)
-            day_num.append(str(cd[1] + "-" + cd[2]))
-        
-        day_num.reverse()
-        bug_num.reverse()
-        
-        graph_data = {
-            "bugs" : bug_num,
-            "day" : day_num
-        }
-        
-        gdf = pd.DataFrame(graph_data, columns=["bugs", "day"])
-        gdf = gdf.groupby("day").agg({"bugs":'sum'}).reset_index()
-        
-        gdf.plot(x ="day", y="bugs", kind = 'bar')
-        plt.show()
-
-
+        cd = row["creation date"].split("T")
+        cd = cd[0].split("-")
+        # Конец цикла как только он вышел за предел искомой даты, дабы не терять время при работе-
+        #- с файлом большого обьема
+        if year > int(cd[0]) and int(cd[1] not in month):
+            break
+        # Сохранение каждого полученого бага и даты его получения в списки
+        if year == int(cd[0]) and int(cd[1]) in month:
+            bugs += 1
+        bug_num.append(bugs)
+        day_num.append(str(cd[2]))
+    
+    # Реверс списков для отображения "прошлое" -> "будущее" по оси х
+    day_num.reverse()
+    bug_num.reverse()
+    
+    graph_data = {
+        "bugs" : bug_num,
+        "day" : day_num
+    }
+    
+    # Создание датафрейма для графика
+    gdf = pd.DataFrame(graph_data, columns=["bugs", "day"])
+    # Если на один день приходится несколько багов, одни объеденяются в одну строку
+    gdf = gdf.groupby("day").agg({"bugs":'sum'}).reset_index()
+    
+    gdf.plot(x ="day", y="bugs", kind = 'bar')
+    plt.show()  
 
 # Последующие функции для создания графиков имеют схожую структуру
     
 def graph_testers(df):
 
+    choice_format = int(input("1 - per quarter, 2 - per month. ? = "))
+
     names = {}
-
-    quarter = int(input("quarter (1-4) = "))
-    year = int(input("year (as 4 digit number) = "))
-    if quarter == 1:
-        month = [1,2,3]
-    elif quarter == 2:
-        month = [4,5,6]
-    elif quarter == 3:
-        month = [7,8,9]
+    
+    if choice_format == 1:
+        quarter = int(input("quarter (1-4) = "))
+        if quarter == 1:
+            month = [1,2,3]
+        elif quarter == 2:
+            month = [4,5,6]
+        elif quarter == 3:
+            month = [7,8,9]
+        else:
+            month = [10,11,12]
     else:
-        month = [10,11,12]
+        monthc = int(input("month (1-12 = "))
+        month = [monthc]
 
+    year = int(input("year (as 4 digit number) = "))
+    
     for index, row in df.iterrows():
         cd = row["creation date"].split("T")
         cd = cd[0].split("-")
@@ -145,16 +119,26 @@ def graph_analytics(df):
 
     components = {}
 
-    quarter = int(input("quarter (1-4) = "))
+    choice_format = int(input("1 - per quarter, 2 - per month. ? = "))
+
+    if choice_format == 1:
+        quarter = int(input("quarter (1-4) = "))    
+    
+        if quarter == 1:
+            month = [1,2,3]
+        elif quarter == 2:
+            month = [4,5,6]
+        elif quarter == 3:
+            month = [7,8,9]
+        else:
+            month = [10,11,12]
+
+    elif choice_format == 2:
+        monthc = int(input("month (1-12 = "))
+        month = [monthc]
+
+
     year = int(input("year (as 4 digit number) = "))
-    if quarter == 1:
-        month = [1,2,3]
-    elif quarter == 2:
-        month = [4,5,6]
-    elif quarter == 3:
-        month = [7,8,9]
-    else:
-        month = [10,11,12]
     
     for index, row in df.iterrows():
         cd = row["creation date"].split("T")
@@ -188,7 +172,7 @@ def graph_analytics(df):
   
 choice = None
 
-# Не забудьте поменять расположение файла для сохранение здесь (строки 192 и 205), и в jira_reqs.py (строка 154)!
+# Не забудьте поменять расположение файла для сохранение здесь (строки 176 и 189), и в jira_reqs.py (строка 186)!
 df = pd.read_csv(r"C:\Users\danil\Documents\Work\jira_reqs\data_csv.csv",engine="python")
 
 
